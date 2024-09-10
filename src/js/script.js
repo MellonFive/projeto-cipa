f65521777e8d9b4fc61c900cca2fd623cf8595c2
Felipe Mellão <felipe.mellao08@gmail.com>
2024-09-10 16:05:43 -0300

    Melhorando comentários

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ const dataDeInicio = new Date(2024, 8, 4); // Exemplo: 4º de Setembro de 2023
 
 // Função para calcular a diferença em dias entre a data de início e a data atual
 function calcularDias() {
-const hoje = new Date(); // Aqui é a  data atual
+const hoje = new Date(); // Aqui é a data atual
 const diferencaDeTempo = Math.abs(hoje - dataDeInicio); // Math.abs é pra deixar o valor sempre positivo
 const diferencaDeDias = Math.floor(diferencaDeTempo / (1000 * 60 * 60 * 24)); // Math.floor é pra sempre arredondar o número, e a operação calcula o número total de dias que se passaram entre o 4 de setembro  e a data atual
 
@@ -16,4 +16,4 @@ const diferencaDeDias = Math.floor(diferencaDeTempo / (1000 * 60 * 60 * 24)); //
 calcularDias()
 
 
-//OBS nos inputs no html coloquei o 'readonly' para que só o javascript mude os dias.
+// OBS nos inputs no html coloquei o 'readonly' para que só o javascript mude os dias.
